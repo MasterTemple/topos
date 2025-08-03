b235434d2c3c819c37fb6b6f4098ff8b22813b4e
Blake Scampone <blake.scampone@gmail.com>
2025-08-02 20:02:03 -0700

    Fixed Roman numeral parsing

--- crates/topos-parser/src/roman_numerals.rs
+++ crates/topos-parser/src/roman_numerals.rs
@@ -18,15 +18,6 @@ pub(crate) fn only_roman_numerals<'a>() -> impl Parser<'a, &'a str, u8> {
         .then_ignore(whitespace())
 }
 
-pub(crate) fn padded_roman_numeral<'a>() -> impl Parser<'a, &'a str, &'a str> {
-    any()
-        .filter(|c: &char| ROMAN_NUMERALS.contains(c))
-        .repeated()
-        .at_least(1)
-        .padded()
-        .to_slice()
-}
-
 /// Only pass it valid values, or it will panic
 fn roman_numeral_value(ch: char) -> u8 {
     match ch {
@@ -40,8 +31,9 @@ fn roman_numeral_value(ch: char) -> u8 {
 }
 
 /// Only pass it valid values, or it will panic
+/// I didn't write this, but it works
 fn parse_roman_numeral(s: &str) -> u8 {
-    let mut res: u8 = 1;
+    let mut res: u8 = 0;
     let mut i = 0;
 
     while i < s.len() {
@@ -54,18 +46,15 @@ fn parse_roman_numeral(s: &str) -> u8 {
 
             // If current value is greater or equal, add it to result
             if s1 >= s2 {
-                // res += s1;
-                res.saturating_add(s1);
+                res = res.saturating_add(s1);
                 i += 1;
             } else {
                 // Else, add the difference and skip next symbol
-                // res += s2 - s1;
-                res.saturating_add(s2 - s1);
+                res = res.saturating_add(s2 - s1);
                 i += 2;
             }
         } else {
-            // res += s1;
-            res.saturating_add(s1);
+            res = res.saturating_add(s1);
             i += 1;
         }
     }
@@ -73,11 +62,16 @@ fn parse_roman_numeral(s: &str) -> u8 {
     res
 }
 
-#[rustfmt::skip]
-#[test]
-fn test_roman_numerals() {
-    let values = [ ("I", 1), ("II", 1), ("III", 1), ("IV", 1), ("V", 1), ("VI", 1), ("VII", 1), ("VIII", 1), ("IX", 1), ("X", 1), ("XI", 1), ("XII", 1), ("XIII", 1), ("XIV", 1), ("XV", 1), ("XVI", 1), ("XVII", 1), ("XVIII", 1), ("XIX", 1), ("XX", 1), ("XXI", 1), ("XXII", 1), ("XXIII", 1), ("XXIV", 1), ("XXV", 1), ("XXVI", 1), ("XXVII", 1), ("XXVIII", 1), ("XXIX", 1), ("XXX", 1), ("XXXI", 1), ("XXXII", 1), ("XXXIII", 1), ("XXXIV", 1), ("XXXV", 1), ("XXXVI", 1), ("XXXVII", 1), ("XXXVIII", 1), ("XXXIX", 1), ("XL", 1), ("XLI", 1), ("XLII", 1), ("XLIII", 1), ("XLIV", 1), ("XLV", 1), ("XLVI", 1), ("XLVII", 1), ("XLVIII", 1), ("XLIX", 1), ("L", 1), ("LI", 1), ("LII", 1), ("LIII", 1), ("LIV", 1), ("LV", 1), ("LVI", 1), ("LVII", 1), ("LVIII", 1), ("LIX", 1), ("LX", 1), ("LXI", 1), ("LXII", 1), ("LXIII", 1), ("LXIV", 1), ("LXV", 1), ("LXVI", 1), ("LXVII", 1), ("LXVIII", 1), ("LXIX", 1), ("LXX", 1), ("LXXI", 1), ("LXXII", 1), ("LXXIII", 1), ("LXXIV", 1), ("LXXV", 1), ("LXXVI", 1), ("LXXVII", 1), ("LXXVIII", 1), ("LXXIX", 1), ("LXXX", 1), ("LXXXI", 1), ("LXXXII", 1), ("LXXXIII", 1), ("LXXXIV", 1), ("LXXXV", 1), ("LXXXVI", 1), ("LXXXVII", 1), ("LXXXVIII", 1), ("LXXXIX", 1), ("XC", 1), ("XCI", 1), ("XCII", 1), ("XCIII", 1), ("XCIV", 1), ("XCV", 1), ("XCVI", 1), ("XCVII", 1), ("XCVIII", 1), ("XCIX", 1), ("C", 1), ("CI", 1), ("CII", 1), ("CIII", 1), ("CIV", 1), ("CV", 1), ("CVI", 1), ("CVII", 1), ("CVIII", 1), ("CIX", 1), ("CX", 1), ("CXI", 1), ("CXII", 1), ("CXIII", 1), ("CXIV", 1), ("CXV", 1), ("CXVI", 1), ("CXVII", 1), ("CXVIII", 1), ("CXIX", 1), ("CXX", 1), ("CXXI", 1), ("CXXII", 1), ("CXXIII", 1), ("CXXIV", 1), ("CXXV", 1), ("CXXVI", 1), ("CXXVII", 1), ("CXXVIII", 1), ("CXXIX", 1), ("CXXX", 1), ("CXXXI", 1), ("CXXXII", 1), ("CXXXIII", 1), ("CXXXIV", 1), ("CXXXV", 1), ("CXXXVI", 1), ("CXXXVII", 1), ("CXXXVIII", 1), ("CXXXIX", 1), ("CXL", 1), ("CXLI", 1), ("CXLII", 1), ("CXLIII", 1), ("CXLIV", 1), ("CXLV", 1), ("CXLVI", 1), ("CXLVII", 1), ("CXLVIII", 1), ("CXLIX", 1), ("CL", 1), ("CLI", 1), ("CLII", 1), ("CLIII", 1), ("CLIV", 1), ("CLV", 1), ("CLVI", 1), ("CLVII", 1), ("CLVIII", 1), ("CLIX", 1), ("CLX", 1), ("CLXI", 1), ("CLXII", 1), ("CLXIII", 1), ("CLXIV", 1), ("CLXV", 1), ("CLXVI", 1), ("CLXVII", 1), ("CLXVIII", 1), ("CLXIX", 1), ("CLXX", 1), ("CLXXI", 1), ("CLXXII", 1), ("CLXXIII", 1), ("CLXXIV", 1), ("CLXXV", 1), ("CLXXVI", 1), ("CLXXVII", 1), ("CLXXVIII", 1), ("CLXXIX", 1), ("CLXXX", 1), ("CLXXXI", 1), ("CLXXXII", 1), ("CLXXXIII", 1), ("CLXXXIV", 1), ("CLXXXV", 1), ("CLXXXVI", 1), ("CLXXXVII", 1), ("CLXXXVIII", 1), ("CLXXXIX", 1), ("CXC", 1), ("CXCI", 1), ("CXCII", 1), ("CXCIII", 1), ("CXCIV", 1), ("CXCV", 1), ("CXCVI", 1), ("CXCVII", 1), ("CXCVIII", 1), ("CXCIX", 1), ("CC", 1), ("CCI", 1), ("CCII", 1), ("CCIII", 1), ("CCIV", 1), ("CCV", 1), ("CCVI", 1), ("CCVII", 1), ("CCVIII", 1), ("CCIX", 1), ("CCX", 1), ("CCXI", 1), ("CCXII", 1), ("CCXIII", 1), ("CCXIV", 1), ("CCXV", 1), ("CCXVI", 1), ("CCXVII", 1), ("CCXVIII", 1), ("CCXIX", 1), ("CCXX", 1), ("CCXXI", 1), ("CCXXII", 1), ("CCXXIII", 1), ("CCXXIV", 1), ("CCXXV", 1), ("CCXXVI", 1), ("CCXXVII", 1), ("CCXXVIII", 1), ("CCXXIX", 1), ("CCXXX", 1), ("CCXXXI", 1), ("CCXXXII", 1), ("CCXXXIII", 1), ("CCXXXIV", 1), ("CCXXXV", 1), ("CCXXXVI", 1), ("CCXXXVII", 1), ("CCXXXVIII", 1), ("CCXXXIX", 1), ("CCXL", 1), ("CCXLI", 1), ("CCXLII", 1), ("CCXLIII", 1), ("CCXLIV", 1), ("CCXLV", 1), ("CCXLVI", 1), ("CCXLVII", 1), ("CCXLVIII", 1), ("CCXLIX", 1), ("CCL", 1), ("CCLI", 1), ("CCLII", 1), ("CCLIII", 1), ("CCLIV", 1), ("CCLV", 1), ("CCLVI", 1), ("CCLVII", 1), ];
-    for (string, int) in values {
-        assert_eq!(parse_roman_numeral(string), int)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[rustfmt::skip]
+    #[test]
+    fn test_roman_numerals() {
+        let values = [ ("I", 1), ("II", 2), ("III", 3), ("IV", 4), ("V", 5), ("VI", 6), ("VII", 7), ("VIII", 8), ("IX", 9), ("X", 10), ("XI", 11), ("XII", 12), ("XIII", 13), ("XIV", 14), ("XV", 15), ("XVI", 16), ("XVII", 17), ("XVIII", 18), ("XIX", 19), ("XX", 20), ("XXI", 21), ("XXII", 22), ("XXIII", 23), ("XXIV", 24), ("XXV", 25), ("XXVI", 26), ("XXVII", 27), ("XXVIII", 28), ("XXIX", 29), ("XXX", 30), ("XXXI", 31), ("XXXII", 32), ("XXXIII", 33), ("XXXIV", 34), ("XXXV", 35), ("XXXVI", 36), ("XXXVII", 37), ("XXXVIII", 38), ("XXXIX", 39), ("XL", 40), ("XLI", 41), ("XLII", 42), ("XLIII", 43), ("XLIV", 44), ("XLV", 45), ("XLVI", 46), ("XLVII", 47), ("XLVIII", 48), ("XLIX", 49), ("L", 50), ("LI", 51), ("LII", 52), ("LIII", 53), ("LIV", 54), ("LV", 55), ("LVI", 56), ("LVII", 57), ("LVIII", 58), ("LIX", 59), ("LX", 60), ("LXI", 61), ("LXII", 62), ("LXIII", 63), ("LXIV", 64), ("LXV", 65), ("LXVI", 66), ("LXVII", 67), ("LXVIII", 68), ("LXIX", 69), ("LXX", 70), ("LXXI", 71), ("LXXII", 72), ("LXXIII", 73), ("LXXIV", 74), ("LXXV", 75), ("LXXVI", 76), ("LXXVII", 77), ("LXXVIII", 78), ("LXXIX", 79), ("LXXX", 80), ("LXXXI", 81), ("LXXXII", 82), ("LXXXIII", 83), ("LXXXIV", 84), ("LXXXV", 85), ("LXXXVI", 86), ("LXXXVII", 87), ("LXXXVIII", 88), ("LXXXIX", 89), ("XC", 90), ("XCI", 91), ("XCII", 92), ("XCIII", 93), ("XCIV", 94), ("XCV", 95), ("XCVI", 96), ("XCVII", 97), ("XCVIII", 98), ("XCIX", 99), ("C", 100), ("CI", 101), ("CII", 102), ("CIII", 103), ("CIV", 104), ("CV", 105), ("CVI", 106), ("CVII", 107), ("CVIII", 108), ("CIX", 109), ("CX", 110), ("CXI", 111), ("CXII", 112), ("CXIII", 113), ("CXIV", 114), ("CXV", 115), ("CXVI", 116), ("CXVII", 117), ("CXVIII", 118), ("CXIX", 119), ("CXX", 120), ("CXXI", 121), ("CXXII", 122), ("CXXIII", 123), ("CXXIV", 124), ("CXXV", 125), ("CXXVI", 126), ("CXXVII", 127), ("CXXVIII", 128), ("CXXIX", 129), ("CXXX", 130), ("CXXXI", 131), ("CXXXII", 132), ("CXXXIII", 133), ("CXXXIV", 134), ("CXXXV", 135), ("CXXXVI", 136), ("CXXXVII", 137), ("CXXXVIII", 138), ("CXXXIX", 139), ("CXL", 140), ("CXLI", 141), ("CXLII", 142), ("CXLIII", 143), ("CXLIV", 144), ("CXLV", 145), ("CXLVI", 146), ("CXLVII", 147), ("CXLVIII", 148), ("CXLIX", 149), ("CL", 150), ("CLI", 151), ("CLII", 152), ("CLIII", 153), ("CLIV", 154), ("CLV", 155), ("CLVI", 156), ("CLVII", 157), ("CLVIII", 158), ("CLIX", 159), ("CLX", 160), ("CLXI", 161), ("CLXII", 162), ("CLXIII", 163), ("CLXIV", 164), ("CLXV", 165), ("CLXVI", 166), ("CLXVII", 167), ("CLXVIII", 168), ("CLXIX", 169), ("CLXX", 170), ("CLXXI", 171), ("CLXXII", 172), ("CLXXIII", 173), ("CLXXIV", 174), ("CLXXV", 175), ("CLXXVI", 176), ("CLXXVII", 177), ("CLXXVIII", 178), ("CLXXIX", 179), ("CLXXX", 180), ("CLXXXI", 181), ("CLXXXII", 182), ("CLXXXIII", 183), ("CLXXXIV", 184), ("CLXXXV", 185), ("CLXXXVI", 186), ("CLXXXVII", 187), ("CLXXXVIII", 188), ("CLXXXIX", 189), ("CXC", 190), ("CXCI", 191), ("CXCII", 192), ("CXCIII", 193), ("CXCIV", 194), ("CXCV", 195), ("CXCVI", 196), ("CXCVII", 197), ("CXCVIII", 198), ("CXCIX", 199), ("CC", 200), ("CCI", 201), ("CCII", 202), ("CCIII", 203), ("CCIV", 204), ("CCV", 205), ("CCVI", 206), ("CCVII", 207), ("CCVIII", 208), ("CCIX", 209), ("CCX", 210), ("CCXI", 211), ("CCXII", 212), ("CCXIII", 213), ("CCXIV", 214), ("CCXV", 215), ("CCXVI", 216), ("CCXVII", 217), ("CCXVIII", 218), ("CCXIX", 219), ("CCXX", 220), ("CCXXI", 221), ("CCXXII", 222), ("CCXXIII", 223), ("CCXXIV", 224), ("CCXXV", 225), ("CCXXVI", 226), ("CCXXVII", 227), ("CCXXVIII", 228), ("CCXXIX", 229), ("CCXXX", 230), ("CCXXXI", 231), ("CCXXXII", 232), ("CCXXXIII", 233), ("CCXXXIV", 234), ("CCXXXV", 235), ("CCXXXVI", 236), ("CCXXXVII", 237), ("CCXXXVIII", 238), ("CCXXXIX", 239), ("CCXL", 240), ("CCXLI", 241), ("CCXLII", 242), ("CCXLIII", 243), ("CCXLIV", 244), ("CCXLV", 245), ("CCXLVI", 246), ("CCXLVII", 247), ("CCXLVIII", 248), ("CCXLIX", 249), ("CCL", 250), ("CCLI", 251), ("CCLII", 252), ("CCLIII", 253), ("CCLIV", 254), ("CCLV", 255), ("CCLVI", 255), ("CCLVII", 255), ];
+        for (string, int) in values {
+            assert_eq!(parse_roman_numeral(string), int)
+        }
     }
 }
